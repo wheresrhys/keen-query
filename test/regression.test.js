313febe92e60e1a3a6e68b444e54c652c3d8ed8b
Rhys Evans <rhys.evans@ft.com>
2016-04-11 13:45:17 +0100

    fixed a few regressions

--- test/regression.test.js
+++ test/regression.test.js
@@ -1,7 +1,9 @@
 const KeenQuery = require('../lib');
+const Aggregator = require('../lib/aggregator');
 const expect = require('chai').expect;
 const fetchMock = require('fetch-mock');
 const mockKeenData = require('./helpers').mockKeenData;
+const sinon = require('sinon');
 // const multiply = require('./helpers').multiply;
 const testQuery = require('./helpers').testQuery;
 
@@ -44,6 +46,26 @@ describe('regression tests', () => {
 					})
 					.then(fetchMock.restore);
 			})
+	})
+
+	it('should not print immediately when building simple query from string', () => {
+		sinon.stub(KeenQuery.prototype, 'print');
+		const kq = KeenQuery.build('potato->count()->group(prop0,prop1)->print(ascii)');
+		expect(KeenQuery.prototype.print.called).to.be.false;
+		expect(kq._printer).to.equal('ascii');
+		KeenQuery.prototype.print.restore();
+	})
+
+	it('should not print immediately when building aggregate query from string', () => {
+		fetchMock
+			.mock(/potato/, mockKeenData(2, 3));
+		sinon.spy(Aggregator.prototype, 'print');
+		const kq = KeenQuery.build('@ratio(potato->count()->group(prop0,prop1),potato->count()->group(prop0,prop1))->print(ascii)');
+		expect(Aggregator.prototype.print.called).to.be.false;
+		kq.print();
+		expect(kq._printer).to.equal('ascii');
+		Aggregator.prototype.print.restore();
+		fetchMock.restore();
 	})
 
 	it('should nest aggregator objects successfully when sharing methods', () => {
