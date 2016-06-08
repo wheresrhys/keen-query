4c90c8ae33b73a8a7bf30482cd0b57b81d861e3d
Laurie Boyes <laurie.boyes@gmail.com>
2016-06-08 14:33:40 +0100

    Fix missing shared functions from aggregator toString (#121)
    * added failing test for aggregator toString preserving filters
    
    * aggregator stringify does all shared functions

---
+++ test/to-string.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const expect = require('chai').expect;
+const KeenQuery = require('../lib/keen-query');
+
+describe('toString', function () {
+	describe('aggregator', function () {
+		it.only('should preserve shared filters', function () {
+
+			const query = KeenQuery.build(`
+				@pct(
+					page:view ->count(user.uuid) ->filter(user.myft.optedIntoDailyEmail=true),
+					page:view->count(user.uuid)
+				)
+				->filter(user.subscriptions.isStaff!=true)`);
+
+			expect(query.toString()).to.equal('@pct(page:view->count(user.uuid)->filter(user.myft.optedIntoDailyEmail=true),page:view->count(user.uuid))->filter(user.subscriptions.isStaff!=true)');
+
+		});
+	});
+});
